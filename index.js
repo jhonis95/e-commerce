2089bc9c68a3f36da2a72018438d50b916e3d48b
jonatan de oliveira <j.jhonis95@gmail.com>
2023-11-23 23:42:49 -0300

    adding main routing to the page

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express= require('express')
-const router=require('./router/main.js')
+const router=require('./src/router/main')
 const app=express();
 const port = 3000
 
 
 app.use('/',router)
+app.use('/admin',router)
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`);
